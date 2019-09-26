5d705b5059f2af9b5644b4117ea8f277a4914242
zname <1093993119@qq.com>
2019-09-26 20:06:40 +0800

    首次上传小程序

---
+++ my-app/weapp/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _core = _interopRequireDefault(require('vendor.js')(0));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_core["default"].app({// 选项
+}, {info: {"noPromiseAPI":["createSelectorQuery"]}, handlers: {}, models: {} });
---
+++ my-app/weapp/pages/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _core = _interopRequireDefault(require('../vendor.js')(0));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_core["default"].page({// 选项
+}, {info: {"components":{},"on":{}}, handlers: {}, models: {} });
